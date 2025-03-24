57f90a1bc6a58206fbf25208b94a14c23e611a65
adimilson pantoja <dimylson@gmail.com>
2025-03-24 20:55:05 -0300

    sellerdaojdbc

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +13,7 @@ public class Main {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21 ,"bob","bog@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
