d5ac45631dc5b1fd174ea3bdc86d63907f44618f
adimilson pantoja <dimylson@gmail.com>
2025-03-24 20:41:18 -0300

    Seller entities class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj.toString());
+
+        Seller seller = new Seller(21 ,"bob","bog@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
